bfea5968130c773da19a4b52b35c2929882d3280
yunfeiyang-l <l591362054@163.com>
2019-08-05 19:54:52 +0800

    lzz

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1> -->
+    <Shopping/>
   </div>
 </template>
+<script>
+import Shopping from "@/components/Shopping.vue";
+export default {
+  name: "about",
+  components: {
+    Shopping
+  }
+};
+</script>
